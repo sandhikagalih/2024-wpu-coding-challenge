9363573fc822da7a7ac887173953ea5cf56ecffb
sandhikagalih <sandhikagalih@gmail.com>
2024-01-18 16:45:28 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,7 @@
+// WPU Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
 // function grow(x) {
 //   let result = x[0];
 //   for (let i = 1; i < x.length; i++) {
