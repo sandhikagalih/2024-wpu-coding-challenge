0515fa75310754ddaa681b35621133f757f4e784
sandhikagalih <sandhikagalih@gmail.com>
2024-01-22 21:58:19 +0700

    day-7

---
+++ 7-countBy.js
@@ -0,0 +1,19 @@
+// WPU Coding Challenge 2024
+// 7/366
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 1; i <= n; i++) {
+//     z.push(i * x);
+//   }
+//   return z;
+// }
+
+// function countBy(x, n) {
+//   return [...Array(n)].map((el, i) => (i + 1) * x);
+// }
+
+const countBy = (x, n) => [...Array(n)].map((_, i) => (i + 1) * x);
+
+console.log(countBy(3, 3));
