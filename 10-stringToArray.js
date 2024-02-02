2aa925b6520bdcb480c1527d6d29ea7cbd2002d7
sandhikagalih <sandhikagalih@gmail.com>
2024-02-02 16:57:02 +0700

    day 10/366

--- 10-stringToArray.js
+++ 10-stringToArray.js
@@ -1,3 +1,7 @@
 // WPU Coding Challenge 2024
 // 10/366
 // https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+const stringToArray = (string) => string.split(' ');
+
+console.log(stringToArray('Robin Singh'));
