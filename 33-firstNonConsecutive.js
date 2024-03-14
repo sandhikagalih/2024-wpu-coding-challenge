0d752982340193fbe5da969a6f6eb5312dc3dc62
sandhikagalih <sandhikagalih@gmail.com>
2024-03-14 22:17:22 +0700

    update #33 menggunakan find

--- 33-firstNonConsecutive.js
+++ 33-firstNonConsecutive.js
@@ -2,17 +2,22 @@
 // 33/366
 // https://www.codewars.com/kata/58f8a3a27a5c28d92e000144
 
+// function firstNonConsecutive(arr) {
+//   let temp = arr[0];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] !== temp + 1) {
+//       return arr[i];
+//     }
+//     temp = arr[i];
+//   }
+
+//   return null;
+// }
+
+// by onecthree, LOVEJOY & mednoob 🔥
 function firstNonConsecutive(arr) {
-  let temp = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] !== temp + 1) {
-      return arr[i];
-    }
-    temp = arr[i];
-  }
-
-  return null;
+  return arr.find((e, i) => e !== arr[0] + i) ?? null;
 }
 
-console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]));
+console.log(firstNonConsecutive([-2, 0]));
