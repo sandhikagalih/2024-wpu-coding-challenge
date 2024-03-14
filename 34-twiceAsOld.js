45886166e4512ab3a201644b8d705d5e55ac346f
sandhikagalih <sandhikagalih@gmail.com>
2024-03-14 22:25:08 +0700

    Day 34/366

---
+++ 34-twiceAsOld.js
@@ -0,0 +1,14 @@
+// WPU Coding Challenge 2024
+// 34/366
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//   // your code here
+//   return Math.abs(dadYearsOld - sonYearsOld * 2);
+// }
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - sonYearsOld * 2);
+
+console.log(
+  twiceAsOld(55, 30) // 0
+);
