f71b8afc0c2ed252b0cecc48cf1e7b9b33928712
sandhikagalih <sandhikagalih@gmail.com>
2024-03-14 21:35:29 +0700

    Day 31/366

---
+++ 31-otherAngle.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 31/366
+// https://www.codewars.com/kata/5a023c426975981341000014
+
+// function otherAngle(a, b) {
+//   return 180 - (a + b);
+// }
+
+const otherAngle = (a, b) => 180 - (a + b);
+
+console.log(otherAngle(30, 60));
