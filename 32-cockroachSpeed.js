5e1701849271b7c3fc26e1393cb9487a5d0f4716
sandhikagalih <sandhikagalih@gmail.com>
2024-03-14 21:48:22 +0700

    tambah ~~ untuk mengganti Math.floor

--- 32-cockroachSpeed.js
+++ 32-cockroachSpeed.js
@@ -6,6 +6,7 @@
 //   return Math.floor((s * 100000) / 3600);
 // }
 
+// const cockroachSpeed = (s) => ~~((s * 100000) / 3600);
 const cockroachSpeed = (s) => Math.floor((s * 100000) / 3600);
 
 console.log(cockroachSpeed(0));
