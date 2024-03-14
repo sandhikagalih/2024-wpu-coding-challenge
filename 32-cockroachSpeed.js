14cb8b5ad6dc327874ab8711daf61077617d574f
sandhikagalih <sandhikagalih@gmail.com>
2024-03-14 21:45:15 +0700

    Day 32/366

---
+++ 32-cockroachSpeed.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 32/366
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+
+// function cockroachSpeed(s) {
+//   return Math.floor((s * 100000) / 3600);
+// }
+
+const cockroachSpeed = (s) => Math.floor((s * 100000) / 3600);
+
+console.log(cockroachSpeed(0));
