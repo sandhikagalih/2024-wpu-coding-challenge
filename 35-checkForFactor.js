bbb3c747fdd6e73229ed620692dd63a3ac6257dd
sandhikagalih <sandhikagalih@gmail.com>
2024-03-20 14:38:15 +0700

    Day 35/366

---
+++ 35-checkForFactor.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// 35/366
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+
+// function checkForFactor(base, factor) {
+//   // return base % factor === 0;
+//   return !(base % factor);
+// }
+
+const checkForFactor = (base, factor) => !(base % factor);
+
+console.log(checkForFactor(24617, 3));
