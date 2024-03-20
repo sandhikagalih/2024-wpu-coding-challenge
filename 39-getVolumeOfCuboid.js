8dc02d3b525080524c20074e97312309831cc3a7
sandhikagalih <sandhikagalih@gmail.com>
2024-03-20 15:08:12 +0700

    Day 39/366

---
+++ 39-getVolumeOfCuboid.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 39/366
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+// class Kata {
+//   static getVolumeOfCuboid(length, width, height) {
+//     return length * width * height;
+//   }
+// }
+
+const getVolumeOfCuboid = (l, w, h) => l * w * h;
