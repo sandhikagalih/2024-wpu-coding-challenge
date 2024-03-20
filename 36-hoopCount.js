573be54946aed89f2ddfc7a24de6313db1f6e5b4
sandhikagalih <sandhikagalih@gmail.com>
2024-03-20 14:43:12 +0700

    Day 36/366

---
+++ 36-hoopCount.js
@@ -0,0 +1,15 @@
+// WPU Coding Challenge 2024
+// 36/366
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+// function hoopCount(n) {
+//   if (n >= 10) {
+//     return 'Great, now move on to tricks';
+//   } else {
+//     return 'Keep at it until you get it';
+//   }
+// }
+
+const hoopCount = (n) => (n >= 10 ? 'Great, now move on to tricks' : 'Keep at it until you get it');
+
+console.log(hoopCount(30));
