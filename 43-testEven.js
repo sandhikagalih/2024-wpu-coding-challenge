7a930283433e1a2abc3e6425d02327e05a239e65
sandhikagalih <sandhikagalih@gmail.com>
2024-08-28 17:22:21 +0700

    Day 43/366

---
+++ 43-testEven.js
@@ -0,0 +1,9 @@
+// WPU Coding Challenge 2024
+// 43/366
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+
+// function testEven(n) {
+//   return n % 2 === 0;
+// }
+
+const testEven = (n) => n % 2 === 0;
